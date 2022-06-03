02f7191a27ba95178d37406690aba1a4994426b1
Danil Kladnitskiy <82809125+danyaisyourhomie@users.noreply.github.com>
2022-06-03 05:21:25 +0300

    update redis

--- bot-manager/src/services.ts
+++ bot-manager/src/services.ts
@@ -3,6 +3,6 @@ import { ClientProxyFactory, Transport } from '@nestjs/microservices';
 export const BOT_ROUTER = ClientProxyFactory.create({
   transport: Transport.REDIS,
   options: {
-    url: 'redis://localhost:6379',
+    url: 'redis://redis:6379',
   },
 });
