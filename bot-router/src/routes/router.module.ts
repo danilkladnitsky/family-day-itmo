075bb5b6df4f9f42f7b8cf96eafd7b54b4b07435
Danil Kladnitskiy <82809125+danyaisyourhomie@users.noreply.github.com>
2022-06-03 04:24:32 +0300

    fix redis

--- bot-router/src/routes/router.module.ts
+++ bot-router/src/routes/router.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
-import { RouteEntity } from 'src/entities/route.entity';
 import { RouterController } from './router.controller';
 import { RouterService } from './router.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [RouterController],
