f9e2574c664c3640016c24477e9c624e76458e49
Danil Kladnitskiy <82809125+danyaisyourhomie@users.noreply.github.com>
2022-06-03 08:28:11 +0300

    add ssl to api host

--- bot-frontend/src/components/common/requests/api.requests.ts
+++ bot-frontend/src/components/common/requests/api.requests.ts
@@ -1,4 +1,4 @@
-export const HOST = process.env.API_HOST ?? " http://itmo.partnadem.com:4000/";
+export const HOST = process.env.API_HOST ?? " https://itmo.partnadem.com:4000/";
 
 export const GET_EDGES = "links";
 export const GET_NODES = "messages";
