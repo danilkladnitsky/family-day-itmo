ffadccceb05098695ef93ea4984948eccdab77df
Danil Kladnitskiy <82809125+danyaisyourhomie@users.noreply.github.com>
2022-06-17 17:44:20 +0300

    include feedback scene

--- bot-manager/src/text/text.module.ts
+++ bot-manager/src/text/text.module.ts
@@ -1,10 +1,11 @@
 import { AskScene } from 'src/scenes/ask.scene';
+import { FeedbackScene } from 'src/scenes/feedback.scene';
 import { FormScene } from 'src/scenes/form.scene';
 import { Module } from '@nestjs/common';
 import { TextService } from './text.service';
 import { TextUpdate } from './text.update';
 
 @Module({
-  providers: [AskScene, FormScene, TextUpdate, TextService],
+  providers: [AskScene, FormScene, FeedbackScene, TextUpdate, TextService],
 })
 export class TextModule {}
